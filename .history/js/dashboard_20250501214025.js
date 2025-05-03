a279eac16ce4d137ba994b0cb13e5f0bbea8a48b
djkeviin <kevingaviria20@hotmail.com>
2025-05-03 02:59:33 -0500

    esta es la rama de la instructora

--- .history/js/dashboard_20250501214025.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const usuario = localStorage.getItem('usuarioActivo');
-    if (!usuario) {
-      alert('Debe iniciar sesión para acceder al panel.');
-      window.location.href = "views/dashboard.html";
-    }
-  });
